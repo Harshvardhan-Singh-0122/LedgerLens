8623c008590eb2af390e219206ffd452b1bf0842
Harshvardhan-Singh-0122 <harshvardhansinghdhannawat@gmail.com>
2026-08-04 19:53:11 +0530

    Fix Gmail SMTP for Render

--- server/src/config/mail.js
+++ server/src/config/mail.js
@@ -1,11 +1,29 @@
+// import nodemailer from "nodemailer";
+
+// const transport = nodemailer.createTransport({
+//     service: "gmail",
+//     auth: {
+//         user: process.env.EMAIL_USER,
+//         pass: process.env.EMAIL_PASS,
+//     },
+// });
+
+// export default transport;
+
+
+//--------for deplyoment-----------
+
 import nodemailer from "nodemailer";
 
 const transport = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-    },
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  family: 4, // Force IPv4
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
 });
 
 export default transport;
