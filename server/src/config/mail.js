189581a9669fc5da2f271d5666b8848cd96adcfc
Harshvardhan-Singh-0122 <harshvardhansinghdhannawat@gmail.com>
2026-08-05 00:49:30 +0530

    Switch email service to Brevo SMTP

--- server/src/config/mail.js
+++ server/src/config/mail.js
@@ -30,25 +30,40 @@
 
 
 
+// import nodemailer from "nodemailer";
+
+// console.log("EMAIL_USER:", process.env.EMAIL_USER);
+// console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
+
+// const transport = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+// });
+
+// transport.verify((error, success) => {
+//   if (error) {
+//     console.error("SMTP Verify Error:", error);
+//   } else {
+//     console.log("SMTP Server is ready");
+//   }
+// });
+
+// export default transport;]
+
+
 import nodemailer from "nodemailer";
 
-console.log("EMAIL_USER:", process.env.EMAIL_USER);
-console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
-
-const transport = nodemailer.createTransport({
-  service: "gmail",
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: Number(process.env.SMTP_PORT),
+  secure: false,
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
   },
 });
 
-transport.verify((error, success) => {
-  if (error) {
-    console.error("SMTP Verify Error:", error);
-  } else {
-    console.log("SMTP Server is ready");
-  }
-});
-
-export default transport;
+export default transporter;
