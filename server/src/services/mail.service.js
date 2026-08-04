a9fa22c20a4f67d98141afc16a27046f32b28219
Harshvardhan-Singh-0122 <harshvardhansinghdhannawat@gmail.com>
2026-08-05 01:22:09 +0530

    Use Brevo Email API

--- server/src/services/mail.service.js
+++ server/src/services/mail.service.js
@@ -1,25 +1,71 @@
-import transporter from "../config/mail.js";
+// import transporter from "../config/mail.js";
 
-export const sendVerificationEmail = async (email, fullName, verificationLink) => {
+// export const sendVerificationEmail = async (email, fullName, verificationLink) => {
 
-    await transporter.sendMail({
-        from: `"LedgerLens" <${process.env.EMAIL_USER}>`,
-        to: email,
-        subject: "Verify your LedgerLens account",
+//     await transporter.sendMail({
+//         from: `"LedgerLens" <${process.env.EMAIL_USER}>`,
+//         to: email,
+//         subject: "Verify your LedgerLens account",
 
-        html: `
-            <h2>Hello ${fullName},</h2>
+//         html: `
+//             <h2>Hello ${fullName},</h2>
 
-            <p>Thank you for registering with LedgerLens.</p>
+//             <p>Thank you for registering with LedgerLens.</p>
 
-            <p>Please click the button below to verify your email.</p>
+//             <p>Please click the button below to verify your email.</p>
 
-            <a href="${verificationLink}">
-                Verify Email
-            </a>
+//             <a href="${verificationLink}">
+//                 Verify Email
+//             </a>
 
-            <p>This link will expire in 24 hours.</p>
-        `,
-    });
+//             <p>This link will expire in 24 hours.</p>
+//         `,
+//     });
 
+// };
+
+
+//----------------for deployment-----------------
+import * as brevo from "@getbrevo/brevo";
+
+const apiInstance = new brevo.TransactionalEmailsApi();
+
+apiInstance.setApiKey(
+  brevo.TransactionalEmailsApiApiKeys.apiKey,
+  process.env.BREVO_API_KEY
+);
+
+export const sendVerificationEmail = async (
+  email,
+  fullName,
+  verificationLink
+) => {
+  await apiInstance.sendTransacEmail({
+    sender: {
+      name: "LedgerLens",
+      email: process.env.EMAIL_USER,
+    },
+    to: [
+      {
+        email: email,
+        name: fullName,
+      },
+    ],
+    subject: "Verify your LedgerLens account",
+    htmlContent: `
+      <h2>Hello ${fullName},</h2>
+
+      <p>Thank you for registering with LedgerLens.</p>
+
+      <p>Please click the button below to verify your email.</p>
+
+      <p>
+        <a href="${verificationLink}">
+          Verify Email
+        </a>
+      </p>
+
+      <p>This link will expire in 24 hours.</p>
+    `,
+  });
 };
